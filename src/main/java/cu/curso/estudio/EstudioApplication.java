a570b4461c0f5d2a93fb7fd61c1b395e8b3b02b6
Nestor Ariel Delgado Pacheco <nestoradp2014@gmail.com>
2021-04-27 18:29:55 -0700

    Subiendo los primeros arhivos

---
+++ src/main/java/cu/curso/estudio/EstudioApplication.java
@@ -0,0 +1,13 @@
+package cu.curso.estudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudioApplication.class, args);
+    }
+
+}
